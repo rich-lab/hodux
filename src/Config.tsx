81d92648a954445733e889e4483b2b3251c0b3cf
chemdemo <yangdemo@gmail.com>
2019-11-27 13:59:39 +0800

    refactor: add event types

--- src/Config.tsx
+++ src/Config.tsx
@@ -1,26 +1,63 @@
 import * as React from 'react';
 import { createContext, PropsWithChildren, createElement, memo } from 'react';
 
-// debugger event types
-export enum EventTypes {
-  set = 'set',
-  add = 'add',
-  delete = 'delete',
-  clear = 'clear',
-  get = 'get',
-  has = 'has',
-  iterate = 'iterate',
+interface GetEvent {
+  type: 'get';
+  target: object;
+  key: string;
+  receiver?: object;
 }
 
-export interface Event {
-  type: EventTypes;
+interface SetEvent {
+  type: 'set';
+  key: string;
   target: object;
-  key?: string;
   receiver?: object;
-  oldValue?: any;
-  value?: any;
-  oldTarget?: object;
+  value: any;
+  oldValue: any;
 }
+
+interface AddEvent {
+  type: 'add';
+  key: string;
+  target: object;
+  receiver?: object;
+  value: any;
+}
+
+interface DeleteEvent {
+  type: 'delete';
+  key: string;
+  target: object;
+  oldValue: any;
+}
+
+interface HasEvent {
+  type: 'has';
+  key: string;
+  target: object;
+}
+
+interface IterateEvent {
+  type: 'iterate';
+  target: object;
+}
+
+interface ClearEvent {
+  type: 'clear';
+  target: object;
+  oldTarget: object;
+}
+
+// debugger event types
+export type Event =
+  | GetEvent
+  | SetEvent
+  | AddEvent
+  | DeleteEvent
+  | HasEvent
+  | IterateEvent
+  | ClearEvent;
 
 export type Config<T = any> = {
   equals?: (a: T, b: T) => boolean;
